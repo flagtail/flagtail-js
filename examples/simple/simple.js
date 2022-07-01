e66b95ffd460f133c869dc04079ae4d082fce760
rhie-coder <quotia72@naver.com>
2022-07-01 19:29:45 +0900

    feat: support array type in PubSub.js about filter
    Now we can use array type when make filter items

--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -1,35 +1,44 @@
 import flagfish from "../../index.js";
 
+const select$$ = [
+    "#id-input", 
+    "#send-btn", 
+    "#result", 
+    ".my-class", 
+    "#clone-part", 
+    "#my-template",
+    "#my-template-second", 
+    "#my-template-third"
+];
+
 (async () => {
     const ff = new flagfish();
 
     const filters = [
         {
-            target: "#my-template", callback: function (data) {
-                console.log("filter ON");
-                console.log(data.children[0]);
+            target: ["#my-template", "#my-template-second", "#my-template-third"], callback: function (data) {
+                console.log(data.children[0].textContent)
                 return data.children[0]
             }
         },
         {
             target: "#id-input", callback: function(data) {
-                console.log("#id-input will be occur error");
-                console.log(data.value);
                 return data
             }
         }
     ]
-
 
     const test = await ff.produce("test")
         .api("test-json", { origin: "https://jsonplaceholder.typicode.com/todos" })
         .api("test-json-1", { origin: "https://jsonplaceholder.typicode.com/todos/1" })
         .api("test-json-2", { origin: "https://jsonplaceholder.typicode.com/todos/2" })
         .api("test-json-3", { origin: "https://jsonplaceholder.typicode.com/todos/3" })
-        .elem("#id-input", "#send-btn", "#result", ".my-class", "#clone-part", "#my-template")
+        .elem(...select$$)
         .filter(filters)
         .done()
 
-    console.log(test)
-    console.log(test.getData())
+    const consumer = ff.consume(test);
+
+    console.log(test.getData());
 })()
+
